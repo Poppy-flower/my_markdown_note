8f77330b8b0a43ce957560bdcc765cddcee79806
ChenHong <2675601413@qq.com>
2018-10-31 11:55:50 +0800

    commit

---
+++ 《javascript 语言精粹》学习笔记/第一章/javascript.js
@@ -0,0 +1,17 @@
+alert('from javascript.js');
+console.log(NaN == NaN);  //false
+console.log(NaN === NaN);  //false
+console.log(isNaN(100));  //false
+console.log(isNaN('abcd'));
+console.log(isNaN('abcd'));
+console.log(isNaN('abcd'));
+console.log(isNaN('abcd'));
+console.log(isNaN('abcd'));
+console.log(isNaN('abcd'));
+console.log(isNaN('abcd'));
+console.log(isNaN('abcd'));
+console.log(isNaN('abcd'));
+console.log('------\\u------');
+console.log('A' === '\u0041');
+console.log('ABCDEFG'.length);
+console.log('c' + 'a' + 't' === 'cat');
