e5d55d19e0a09e4716c67d0b675a6245ad74f1db
ChenHong <2675601413@qq.com>
2018-11-07 21:15:02 +0800

    commit 判断日期有效性

--- 小砖科技面试题/判断日期有效性.js
+++ 小砖科技面试题/判断日期有效性.js
@@ -5,6 +5,47 @@
  * @param d
  */
 
-function checkDate(y, m, d){
-
+function checkYear(y){
+    return (y-0 >= 0);
 }
+
+function checkMonth(m){
+    return (m-0 > 0) && (m-0 <= 12);
+}
+
+
+
+function checkOtherMonth(m, d){
+    var m = m-0, d = d-0;
+    if(m === 4 || m === 6 || m===9 || m===11){
+        return (d > 0 && d <= 30);
+    }else{
+        return (d > 0 && d <= 31);
+    }
+}
+
+
+function checkDay(y, m, d){
+    if(y%4 === 0 && y%100  !== 0){   //闰年，能被4整除，且不能被100整除
+        if(m-0 === 2){
+            return (m-0 >0) && (m-0 <=29);
+        }else{
+            return checkOtherMonth(m, d);
+        }
+    }else{
+        if(m-0 === 2){
+            return (m-0 >0) && (m-0 <=28);
+        }else{
+            return checkOtherMonth(m, d);
+        }
+    }
+}
+
+function checkDate(y, m, d){
+    if(checkYear(y) && checkMonth(m) && checkDay(y, m, d)){
+        return true;
+    }
+    return false;
+}
+
+console.log(checkDate(2006, 10, 31));   //true
